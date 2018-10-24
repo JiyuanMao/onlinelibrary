f88312823aef287721dc87c84775cccb594426f9
Jiyuan Mao <1078120230@qq.com>
2018-10-24 17:58:48 +0100

    Edit comments endpoint tested

--- test/routes/comments-test.js
+++ test/routes/comments-test.js
@@ -6,43 +6,44 @@ let expect = chai.expect;
 chai.use(chaiHttp);
 let _ = require('lodash' );
 
-describe('Comments', function (){
+describe('Comments', function () {
     // TODO
-    describe('GET /comments/:bookname',  () => {
-        it('should return the comments you search for', function(done) {
+    describe('GET /comments/:bookname', () => {
+        it('should return the comments you search for', function (done) {
             chai.request(server)
                 .get('/comments/Foundations for Analytics with Python')
-                .end(function(err, res) {
+                .end(function (err, res) {
                     expect(res).to.have.status(200);
                     expect(res.body).to.be.a('array');
                     expect(res.body.length).to.equal(2);
                     let result = _.map(res.body, (comment) => {
                         return {
                             text: comment.text,
-                            username:comment.username,
+                            username: comment.username,
                             bookname: comment.bookname,
                         }
                     });
-                    expect(result).to.include( {  text: "it is very useful ",
+                    expect(result).to.include({
+                        text: "it is very useful ",
                         bookname: "Foundations for Analytics with Python",
                         username: "john"
-                    } );
+                    });
 
                     done();
                 });
         });
-        it('should return a 404 and a message for invalid book name', function(done) {
+        it('should return a 404 and a message for invalid book name', function (done) {
             chai.request(server)
                 .get('/comments/abc')
-                .end(function(err, res) {
+                .end(function (err, res) {
                     expect(res).to.have.status(404);
-                    expect(res.body).to.have.property('message','Comment NOT Found!' ) ;
+                    expect(res.body).to.have.property('message', 'Comment NOT Found!');
                     done();
                 });
         });
     });
     describe('POST /comments', function () {
-        it('should return confirmation message and update datastore', function(done) {
+        it('should return confirmation message and update datastore', function (done) {
             let comment = {
                 text: "it needs improvements",
                 bookname: "Foundations for Analytics with Python",
@@ -51,28 +52,46 @@ describe('Comments', function (){
             chai.request(server)
                 .post('/comments')
                 .send(comment)
-                .end(function(err, res) {
+                .end(function (err, res) {
                     expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('message').equal('Comment Successfully Added!' );
+                    expect(res.body).to.have.property('message').equal('Comment Successfully Added!');
                     done();
                 });
         });
-        after(function  (done) {
+        after(function (done) {
             chai.request(server)
                 .get('/comments/Foundations for Analytics with Python')
-                .end(function(err, res) {
+                .end(function (err, res) {
                     let result = _.map(res.body, (comment) => {
-                        return { text: comment.text,
-                            username:comment.username,
+                        return {
+                            text: comment.text,
+                            username: comment.username,
                             bookname: comment.bookname,
                         };
-                    }  );
-                    expect(result).to.include( { text: "it needs improvements",
+                    });
+                    expect(result).to.include({
+                        text: "it needs improvements",
                         bookname: "Foundations for Analytics with Python",
                         username: "justin"
-                    } );
+                    });
                     done();
                 });
         });
     });
-});
+    describe.only('PUT /comments/:id', () => {
+        it('should return the updated message', function (done) {
+            let comment = {
+                text: "it needs improvements",
+                bookname: "Digital Portrait Photography For Dummies",
+                username: "justin"
+            };
+            chai.request(server)
+                .put('/books/5bd09713d78fca279c9a7981')
+                .send(comment)
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    done();
+                });
+        });
+    });
+})
